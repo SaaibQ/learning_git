12a1eb4fb3b946a9ab83fdfc9ca546b103e7a13c
Saaib Qazi <saaib.qazi@iquasar.com>
2023-10-06 19:11:30 +0530

    Verion 1

---
+++ script1.js
@@ -0,0 +1,25 @@
+"use strict";
+const newFeature1 = function () {
+  console.log("New feature1");
+};
+
+const newFeature2 = function () {
+  console.log("New feature2");
+};
+
+const newFeature3 = function () {
+  console.log("New feature3");
+};
+
+const newFeature4 = function () {
+  console.log("New feature4");
+};
+
+const init = function () {
+  newFeature1();
+  newFeature2();
+  newFeature3();
+  newFeature4();
+};
+
+init();
